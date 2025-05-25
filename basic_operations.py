a7f0d804bcff3752ead048177b7a544df78fc863
Bez-coder <149112199+Bez-coder@users.noreply.github.com>
2025-05-25 14:30:57 +0300

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,8 +1,13 @@
-number1=10
-number2=5
-sum = number1+number2
-difference=number1-number2
-product=number1*number2
-print(f"Addition of {number1} and {number2} is {sum}" )
-print(f"Subtraction of {number1} and {number2} is {difference}" )
-print(f"Multiplication of {number1} and {number2} is {product}" )
+# Define variables
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print the results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
